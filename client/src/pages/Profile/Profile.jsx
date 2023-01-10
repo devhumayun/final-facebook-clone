d38122e1f148444d48c00468dec088049cfd2a1d
devhumayun <humayunkabir9408@gmail.com>
2023-01-10 16:42:13 +0600

    User Profile added

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import CreatePost from '../../components/CreatePost/CreatePost';
+import HomeHeader from '../../components/HomeHeader/HomeHeader';
+import ProfileHeader from '../../components/Profile/ProfileHeader/ProfileHeader';
+import UserPost from '../../components/UserPost/UserPost';
 
 const Profile = () => {
   return (
     <>
-    
-      <span> Profile </span>
+       <HomeHeader />
+       <ProfileHeader />
+       <div class="fb-profile-body">
+         <div className="fb-profile-wrapper">
+          <div className="fb-profile-left">
+            test
+          </div>
+          <div className="fb-profile-right">
+            <CreatePost />
+            <UserPost />
+          </div>
+         </div>
+       </div>
 
     </>
   )
