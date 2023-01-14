51884521e26ff069dc6d464f691f18d8e81c4008
devhumayun <humayunkabir9408@gmail.com>
2023-01-14 16:51:04 +0600

    profile page update

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -82,10 +82,13 @@ const Profile = () => {
                   <button className='edit-button'> Edit Featured </button>
                </div>
                <div className="gallery-img-box">
-                  <div className="gallery-img">
                      <div className="gallery-header">
                         <h3> Photos </h3>
                         <a href="#"> See all photos </a>
+                     </div>
+                  <div className="gallery-img">
+                     <div className="gallery-item">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfGngqhUHTdoUH_hrxRS66jNK4XYRy0AwNU6laJO6y0NWfHN_ngNjIC6Zs4DnhkyWrNVs&usqp=CAU" alt="" />
                      </div>
                      <div className="gallery-item">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfGngqhUHTdoUH_hrxRS66jNK4XYRy0AwNU6laJO6y0NWfHN_ngNjIC6Zs4DnhkyWrNVs&usqp=CAU" alt="" />
