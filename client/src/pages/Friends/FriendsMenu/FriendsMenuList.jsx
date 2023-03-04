3cbd2c41363057e60181dddf5caa6d3bc3b04062
devhumayun <humayunkabir9408@gmail.com>
2023-03-04 17:21:35 +0600

    start work in friends page

--- client/src/pages/Friends/FriendsMenu/FriendsMenuList.jsx
+++ client/src/pages/Friends/FriendsMenu/FriendsMenuList.jsx
@@ -4,7 +4,18 @@ import './FriendsMenuList.scss'
 const FriendsMenuList = () => {
   return (
     <div className="friends-menu-list">
-      <div className="friends-menu-wraper"></div>
+      <div className="friends-menu-wraper">
+        <div className="menu-header">
+          <h2>Friends</h2>
+          <div
+            style={{
+              backgroundImage:
+                'url(https://static.xx.fbcdn.net/rsrc.php/v3/yT/r/3RT1RtjDYAd.png?_nc_eui2=AeEB12UNW2voTCaZKJD4Ct7vLuZLQ50Zseku5ktDnRmx6RnvHR5NYRdgIQwdJb4XIJIWvzZoWM-WT2Pb2e9tYGXN)',
+            }}
+            className="setting-noti"
+          ></div>
+        </div>
+      </div>
     </div>
   )
 }
